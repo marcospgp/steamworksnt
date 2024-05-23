f7885e505332f0bdd1b1032618f0f35d1cde28b6
Marcos Pereira <3464445+marcospgp@users.noreply.github.com>
2024-05-23 23:07:06 +0100

    Update Steamworks.cs

--- Steamworks.cs
+++ Steamworks.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Globalization;
 using System.Runtime.InteropServices;
 using Steamworksnt.SteamworksApi;
 using UnityEngine;
@@ -7,49 +8,51 @@ namespace Steamworksnt
 {
     public static class Steamworks
     {
-        /// <summary>
-        /// Init Steamworks SDK before any code has a chance to call it.
-        /// </summary>
-        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
-        private static void Init()
+        // /// <summary>
+        // /// Init Steamworks SDK before any code has a chance to call it.
+        // /// </summary>
+        // [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.AfterAssembliesLoaded)]
+        // private static void Init()
+        // {
+        //     // Shut down Steamworks SDK when quitting.
+        //     Application.quitting += () => Api.SteamAPI_Shutdown();
+
+        //     if (Api.SteamAPI_RestartAppIfNecessary())
+        //     {
+        //         UnityEngine.Debug.Log(
+        //             "\"SteamAPI_RestartAppIfNecessary()\" returned true. Steam should "
+        //                 + "now be starting a new process, so terminating this one..."
+        //         );
+        //         Application.Quit();
+        //         return;
+        //     }
+
+        //     if (!Api.SteamAPI_IsSteamRunning())
+        //     {
+        //         Fail("Unable to init Steam SDK because the Steam client is not running.");
+        //     }
+
+        //     if (!Api.SteamAPI_Init())
+        //     {
+        //         Fail("Unable to init Steam SDK. See logs for more details.");
+        //     }
+
+        //     // Hook warning & debug messages
+        //     Api.SteamAPI_ISteamUtils_SetWarningMessageHook(
+        //         Api.SteamAPI_SteamUtils_v010(),
+        //         Steamworks.OnWarningMessage
+        //     );
+
+        //     Callbacks.Init();
+
+        //     UnityEngine.Debug.Log("Steamworks SDK init successful.");
+        // }
+
+        private static void OnWarningMessage(int nSeverity, IntPtr pchDebugText)
         {
-            // Shut down Steamworks SDK when quitting.
-            Application.quitting += () => Api.SteamAPI_Shutdown();
-
-            if (Api.SteamAPI_RestartAppIfNecessary())
-            {
-                UnityEngine.Debug.Log(
-                    "\"SteamAPI_RestartAppIfNecessary()\" returned true. Steam should "
-                        + "now be starting a new process, so terminating this one..."
-                );
-                Application.Quit();
-                return;
-            }
-
-            if (!Api.SteamAPI_IsSteamRunning())
-            {
-                Fail("Unable to init Steam SDK because the Steam client is not running.");
-            }
-
-            if (!Api.SteamAPI_Init())
-            {
-                Fail("Unable to init Steam SDK. See logs for more details.");
-            }
-
-            // Hook warning & debug messages
-            Api.SteamAPI_ISteamUtils_SetWarningMessageHook(
-                Api.SteamAPI_SteamUtils_v010(),
-                Steamworks.OnWarningMessage
+            UnityEngine.Debug.LogWarning(
+                $"Steamworks SDK warning (severity {nSeverity.ToString(CultureInfo.InvariantCulture)}):"
             );
-
-            Callbacks.Init();
-
-            UnityEngine.Debug.Log("Steamworks SDK init successful.");
-        }
-
-        private static void OnWarningMessage(Int32 nSeverity, IntPtr pchDebugText)
-        {
-            UnityEngine.Debug.LogWarning($"Steamworks SDK warning (severity {nSeverity}):");
             UnityEngine.Debug.LogWarning(Marshal.PtrToStringUni(pchDebugText));
         }
 
